98d302b860cfc0a1d53cfbe27e7628f87a69ff67
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-01-07 11:18:56 -0300

    query client-query client provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import  App  from './App'
+import App from './App'
 import './index.css'
+import { QueryClientProvider } from '@tanstack/react-query'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <QueryClientProvider>
     <App />
+
+    </QueryClientProvider>
   </React.StrictMode>,
 )
