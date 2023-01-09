e21821895664eb97977ba7cf481a3677c62fdce0
pyvan23 <ivanmiguelalvarez@gmail.com>
2023-01-09 17:10:39 -0300

    repo list

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,18 +1,30 @@
 import { Repos } from "../types/types"
 import { useFavouriteRepo } from '../store/reposList';
 
+
 interface Card {
     repo: Repos,
     isFavorite: boolean
 }
 
 export const Cards = ({ repo, isFavorite }: Card) => {
-    const {addFavoriteRepo,deleteFavoriteRepo} = useFavouriteRepo()
+
+    const addFavoriteRepo = useFavouriteRepo(state => state.addFavoriteRepo)
+    const deleteFavoriteRepo = useFavouriteRepo(state => state.deleteFavoriteRepo)
+
+    const toggleFavorites = () => {
+        if (isFavorite) {
+            deleteFavoriteRepo(repo.id)
+            return
+        }
+        addFavoriteRepo(repo.id)
+    }
+
 
     return (
         <div>
             <h1>{repo.name}</h1>
-            <button>{ isFavorite ? 'dislike' : 'like' }</button>
+            <button onClick={toggleFavorites}>{isFavorite ? 'dislike' : 'like'}</button>
         </div>
     )
 }
--- src/store/reposList.ts
+++ src/store/reposList.ts
@@ -1,24 +1,30 @@
 import create from "zustand";
+import { persist } from "zustand/middleware";
 
 type favoriteReposState = {
-    favoritesReposId: number[],
-    addFavoriteRepo: (id: number) => void,
-    deleteFavoriteRepo: (id: number) => void,
-}
+  favoritesReposId: number[];
+  addFavoriteRepo: (id: number) => void;
+  deleteFavoriteRepo: (id: number) => void;
+};
 
+export const useFavouriteRepo = create(
+  persist<favoriteReposState>(
+    (set) => ({
+      favoritesReposId: [],
+      addFavoriteRepo: (id: number) =>
+        set((state) => ({
+          favoritesReposId: [...state.favoritesReposId, id],
+        })),
 
-export const useFavouriteRepo = create<favoriteReposState>((set) => ({
-    favoritesReposId: [],
-    addFavoriteRepo: (id: number) => set((state) => ({
-        favoritesReposId: [...state.favoritesReposId, id]
-    })),
-    
-    deleteFavoriteRepo: (id: number) => set(state => ({
-        favoritesReposId: state.favoritesReposId.filter(repoId => repoId !== id)
-    }))
-
-}
-
-
-}))
-
+      deleteFavoriteRepo: (id: number) =>
+        set((state) => ({
+          favoritesReposId: state.favoritesReposId.filter(
+            (repoId) => repoId !== id
+          ),
+        })),
+    }),
+    {
+      name: "favorite-repos",
+    }
+  )
+);
